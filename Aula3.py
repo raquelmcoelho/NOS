8331e435c03a99069f0b6fd065ea20de753c4da7
raquel <raquelmcoelhodesousa@gmail.com>
2021-03-23 19:14:08 -0300

    terceira aula - unicode

--- Aula3.py
+++ Aula3.py
@@ -1,37 +1,30 @@
-"""AULA 03 ENCODING
-#1. Dada a string “A persistência é o caminho do êxito” codifique-a utilizando UTF-8
-e UTF-16.
-1.1.O que acontece em cada caso? Justifique. R- mulher codifica para bytes no padrão de cada respectiva utf
-1.2.Qual a diferença de tamanho entre as saídas em UTF-8 e UTF-16? grande
-1.3.Tente realizar a codificação/decodificação cruzada. O que acontece?
+#AULA 03 ENCODING
+#PRIMEIRA QUESTÃO
+print("1. Dada a string “A persistência é o caminho do êxito” codifique-a utilizando UTF-8 e UTF-16")
 a = "A persistência é o caminho do êxito"
 b = a.encode()
 c = a.encode("utf-16")
-b= b.decode() #decodificar
 print("\noriginal:", a,"\nutf-8:", b ,"\nutf-16:", c)
+b= b.decode() #decodificar
+c= c.decode("utf-16")
+print("\ndecodificados:\nutf-8:", b, "\nutf-16:",c)
 
-2. Decodifique a sequência de bytes:
-2.1.
 
-print(b'\xff\xfeA\x00 \x00p\x00e\x00r\x00s\x00i\x00s\x00t\x00\xea\x00n\x00c\x00i\x00a\x00 \x00\xe9\x00 \x00o\x00 \x00c\x00a\x00m\x00i\x00n\x00h\x00o\x00 \x00d\x00o\x00 \x00\xea\x00x\x00i\x00t\x00o\x00'.decode("utf-16"))
+#SEGUNDA QUESTÃO
+print("\n\n", r"2. Decodifique a sequência de bytes: \xff\xfeA\x00 \x00p\x00e\x00r\x00s\x00i\x00s\x00t\x00\xea\x00n\x00c\x00i\x00a\x00 \x00\xe9\x00 \x00o\x00 \x00c\x00a\x00m\x00i\x00n\x00h\x00o\x00 \x00d\x00o\x00 \x00\xea\x00x\x00i\x00t\x00o\x00")
+d = b'\xff\xfeA\x00 \x00p\x00e\x00r\x00s\x00i\x00s\x00t\x00\xea\x00n\x00c\x00i\x00a\x00 \x00\xe9\x00 \x00o\x00 \x00c\x00a\x00m\x00i\x00n\x00h\x00o\x00 \x00d\x00o\x00 \x00\xea\x00x\x00i\x00t\x00o\x00'
+print("decodificado:", d.decode("utf-16"))
 
-2.2.Qual seu conteúdo? Foi possível decodificar? Qual a codificação correta?
-A persistencia é o cmainho de exito, utf-16
+#TERCEIRA QUESTÃO
+print("\n\n", r"3.Tente decodificar a nova sequência:b'\xff\xfeA\x00p\x00r\x00e\x00n\x00d\x00e\x00n\x00d\x00o\x00 \x00p\x00r\x00o\x00g\x00r\x00a\x00m\x00a\x00\xe7\x00\xe3\x00o\x00 \x00e\x00m\x00 \x00P\x00y\x00t\x00h\x00o\x00n\x00'")
+print("decodificado:", b'\xff\xfeA\x00p\x00r\x00e\x00n\x00d\x00e\x00n\x00d\x00o\x00 \x00p\x00r\x00o\x00g\x00r\x00a\x00m\x00a\x00\xe7\x00\xe3\x00o\x00 \x00e\x00m\x00 \x00P\x00y\x00t\x00h\x00o\x00n\x00'.decode("utf-16"))
 
-3. Tente decodificar a nova sequência:
-b'
-print(b'\xff\xfeA\x00p\x00r\x00e\x00n\x00d\x00e\x00n\x00d\x00o\x00 \x00p\x00r\x00o\x00g\x00r\x00a\x00m\x00a\x00\xe7\x00\xe3\x00o\x00 \x00e\x00m\x00 \x00P\x00y\x00t\x00h\x00o\x00n\x00'.decode("utf-16"))
-aprendendo programação em python
 
-4. O que acontece se for utiliza a codificação errada? Explique o erro mostrado.
-dá erro ou mapeia errado
+#QUARTA QUESTÃO
+print("\n\nComo evitar o erro (UnicodeDecodeError:) utilizando o parâmetro ‘replace' ou ‘ignore' do método encode? ")
 
-5. Como evitar o erro (UnicodeDecodeError:) do ítem anterior utilizando o
-parâmetro ‘replace' ou ‘ignore' do método encode? Pesquise no site
-python.org. Diferencie-os.
 txt= b'\xff\xfeA\x00p\x00r\x00e\x00n\x00d\x00e\x00n\x00d\x00o\x00 \x00p\x00r\x00o\x00g\x00r\x00a\x00m\x00a\x00\xe7\x00\xe3\x00o\x00 \x00e\x00m\x00 \x00P\x00y\x00t\x00h\x00o\x00n\x00'
 print(txt.decode("utf-8", errors="replace"))
 print(txt.decode("utf-8", errors="ignore"))
-"""
 
 
